baecf3c2ef47fe6c974a6b3bfefe45a97ce4a67e
KhinNweTun <>
2021-09-04 20:01:51 +0630

    test

---
+++ Chapter5.sql
@@ -0,0 +1,7 @@
+select concat('Hello, ', 'World') from dual;
+
+select concat(concat('Hello, ', 'World '), concat('Greate to ', 'see you.')) from dual;
+
+select 'Hello, ' || 'World' from dual;
+
+select 'Hello, ' || 'World! ' || 'Greate to ' || 'see you.' from dual;
